89ad5e29f4b3afb5c56e160c0212c6a2b5f249a9
vinna8 <90774751+vinna8@users.noreply.github.com>
2022-03-26 19:28:34 +0700

    commit

--- src/hoc/withAuthNavigate.js
+++ src/hoc/withAuthNavigate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 
 export const withAuthNavigate = (Component) => {
